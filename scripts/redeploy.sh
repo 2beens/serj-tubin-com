2f2e81459be63825bcf0498e69cea1c7a3f31270
serj <srdjan@adjust.com>
2020-10-11 10:49:27 +0200

    redeploy script added

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -1,7 +1,35 @@
 #!/usr/bin/env bash
+set -o pipefail # abort on errors in pipeline
+set -e          # abort on errors
 
-# TODO:
+# TODO: maybe read the branch name from stdin
+# read -p "branch name: " branch
+
+branch="master"
+case $1 in
+    "master")
+        ;;
+    "")
+        ;;
+    "-c")
+        ;& # fallthru
+    "--current")
+        branch=$(git rev-parse --abbrev-ref HEAD)
+        ;;
+    *)
+        branch=$1
+esac
+
 #   1 get branch name
+echo "deploying branch:" "${branch}"
+
 #   2 checkout branch
-#   3 build project
-#   4 restart service [sudo systemctl restart ...]
+git fetch --all
+git checkout ${branch}
+git rebase
+
+# build project
+go build cmd/main.go
+# restart service and show info
+sudo systemctl restart serj-tubin-backend.service
+sudo systemctl status serj-tubin-backend.service
