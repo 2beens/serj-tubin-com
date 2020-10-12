94f198ef4bb51aa9c3770d4ea82e2378785516c9
serj <srdjan@adjust.com>
2020-10-12 22:16:31 +0200

    added aero setup - create secondary board messages index

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	"os"
 	"strings"
-
-	log "github.com/sirupsen/logrus"
+	"time"
 
 	"github.com/2beens/serjtubincom/internal"
+	as "github.com/aerospike/aerospike-client-go"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -21,7 +22,19 @@ func main() {
 	aeroNamespace := flag.String("aero-namespace", "serj-tubin-com", "aerospike namespace value (used in aerospike server)")
 	port := flag.Int("port", 8080, "port number")
 	logsPath := flag.String("logs-path", "", "server logs file path (empty for stdout)")
+	aeroSetup := flag.Bool("aero-setup", false, "run aerospike db setup")
+
 	flag.Parse()
+
+	if *aeroSetup {
+		if err := setupAeroDb(*aeroNamespace, *aeroHost, *aeroPort); err != nil {
+			fmt.Printf("aero setup failed: %s\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("aero setup complete")
+		os.Exit(0)
+	}
+
 	log.Debugf("using port: %d", *port)
 	log.Debugf("using server logs path: %s", *logsPath)
 
@@ -39,6 +52,54 @@ func main() {
 	if server != nil {
 		server.Serve(*port)
 	}
+}
+
+func setupAeroDb(namespace, host string, port int) error {
+	fmt.Println("staring aero setup ...")
+
+	aeroClient, err := as.NewClient(host, port)
+	if err != nil {
+		return fmt.Errorf("failed to create aero client: %w", err)
+	}
+
+	// TODO: maybe drop index first ?
+	// aeroClient.DropIndex(...)
+
+	if err := createBoardMessagesSecondaryIndex(aeroClient, namespace); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func createBoardMessagesSecondaryIndex(aeroClient *as.Client, namespace string) error {
+	task, err := aeroClient.CreateIndex(
+		nil,
+		namespace,
+		"messages",
+		"id_index",
+		"id",
+		as.NUMERIC,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to get create index task: %w", err)
+	}
+
+	waitSecondsMax := 20
+	for i := 0; i < waitSecondsMax; i++ {
+		if done, err := task.IsDone(); err != nil {
+			fmt.Println(".")
+		} else if done {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+
+	if err = <-task.OnComplete(); err != nil {
+		return fmt.Errorf("failed to create index: %w", err)
+	}
+
+	return nil
 }
 
 func loggingSetup(logFileName string, logLevel string) {
