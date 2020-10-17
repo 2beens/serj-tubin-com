bc7a6a3fcc02610845c9ea60506843471d3d5da8
serj <srdjan@adjust.com>
2020-10-17 11:16:20 +0200

    8 board aero test client implemented

--- internal/aerospike/board_aero_test_client.go
+++ internal/aerospike/board_aero_test_client.go
@@ -19,20 +19,62 @@ func NewBoardAeroTestClient() *BoardAeroTestClient {
 	}
 }
 
+func NewBoardAeroTestClientWithBins(aeroBinMaps map[string]AeroBinMap) *BoardAeroTestClient {
+	return &BoardAeroTestClient{
+		aeroBinMaps: aeroBinMaps,
+	}
+}
+
 func (tc *BoardAeroTestClient) Put(key string, binMap AeroBinMap) error {
-	panic("implement me")
+	switch {
+	case key == "":
+		return errors.New("empty key")
+	case binMap == nil:
+		return errors.New("nil bin map")
+	}
+	tc.aeroBinMaps[key] = binMap
+	return nil
 }
 
 func (tc *BoardAeroTestClient) Delete(key string) (bool, error) {
-	panic("implement me")
+	if key == "" {
+		return false, errors.New("empty key")
+	}
+
+	_, found := tc.aeroBinMaps[key]
+	if !found {
+		return false, nil
+	}
+
+	delete(tc.aeroBinMaps, key)
+
+	return true, nil
 }
 
 func (tc *BoardAeroTestClient) QueryByRange(index string, from, to int64) ([]AeroBinMap, error) {
-	panic("implement me")
+	var binMaps []AeroBinMap
+	for _, binMap := range tc.aeroBinMaps {
+		val, indexFound := binMap[index]
+		if !indexFound {
+			continue
+		}
+		valInt64, ok := val.(int64)
+		if !ok {
+			continue
+		}
+		if valInt64 >= from && valInt64 <= to {
+			binMaps = append(binMaps, binMap)
+		}
+	}
+	return binMaps, nil
 }
 
 func (tc *BoardAeroTestClient) ScanAll() ([]AeroBinMap, error) {
-	panic("implement me")
+	var binMaps []AeroBinMap
+	for _, binMap := range tc.aeroBinMaps {
+		binMaps = append(binMaps, binMap)
+	}
+	return binMaps, nil
 }
 
 func (tc *BoardAeroTestClient) CountAll() (int, error) {
@@ -43,7 +85,7 @@ func (tc *BoardAeroTestClient) CountAll() (int, error) {
 }
 
 func (tc *BoardAeroTestClient) IsConnected() bool {
-	panic("implement me")
+	return true
 }
 
 func (tc *BoardAeroTestClient) Close() {
