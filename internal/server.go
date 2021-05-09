7d66b8ef3fee3ded1ec36dd69485f976f3ec22d8
serj <srdjan@adjust.com>
2021-05-09 09:48:47 +0200

    small CORS updates

--- internal/server.go
+++ internal/server.go
@@ -96,12 +96,10 @@ func NewServer(
 		admin:                 admin,
 	}
 
-	qm, err := NewQuoteManager("./assets/quotes.csv")
+	s.quotesManager, err = NewQuoteManager("./assets/quotes.csv")
 	if err != nil {
 		return nil, fmt.Errorf("failed to create quote manager: %s", err)
 	}
-
-	s.quotesManager = qm
 
 	return s, nil
 }
@@ -192,10 +190,13 @@ func (s *Server) gracefulShutdown(httpServer *http.Server) {
 func (s *Server) corsMiddleware() func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// TODO: fix to allow only "www.serj-tubin.com"
+
 			//Allow CORS here By * or specific origin
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
 			next.ServeHTTP(w, r)
 		})
 	}
