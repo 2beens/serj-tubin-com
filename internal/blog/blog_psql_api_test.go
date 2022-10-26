888c6f981c35046e6e7bd3a816b6b9b355b36a51
serj.tubin <stubin87@gmail.com>
2022-10-26 12:04:22 +0200

    TestPsqlApi_AddBlog added

--- internal/blog/blog_psql_api_test.go
+++ internal/blog/blog_psql_api_test.go
@@ -27,3 +27,36 @@ func TestNewBlogPsqlApi(t *testing.T) {
 	require.NotNil(t, psqlApi)
 	assert.NotNil(t, psqlApi.db)
 }
+
+func TestPsqlApi_AddBlog(t *testing.T) {
+	ctx := context.Background()
+	timeoutCtx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+
+	host := os.Getenv("POSTGRES_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	t.Logf("using postres host: %s", host)
+
+	psqlApi, err := NewBlogPsqlApi(timeoutCtx, host, "5432", "serj_blogs")
+	require.NoError(t, err)
+
+	blogsCount, err := psqlApi.BlogsCount(ctx)
+	require.NoError(t, err)
+
+	err = psqlApi.AddBlog(ctx, &Blog{
+		Title:   "b1",
+		Content: "content1",
+	})
+	require.NoError(t, err)
+	err = psqlApi.AddBlog(ctx, &Blog{
+		Title:   "b2",
+		Content: "content2",
+	})
+	require.NoError(t, err)
+
+	blogsCountAfter, err := psqlApi.BlogsCount(ctx)
+	require.NoError(t, err)
+	assert.Equal(t, 2+blogsCount, blogsCountAfter)
+}
