5b21e807616e70fe89aee7cd04a9b2a8b1012f33
serj <stubin87@gmail.com>
2022-10-27 13:54:32 +0200

    netlog psql api integration tests init; TestPsqlApi_AddVisit

--- internal/netlog/psql_api_test.go
+++ internal/netlog/psql_api_test.go
@@ -1,10 +1,32 @@
+//go:build integration_test || all_tests
+
 package netlog
 
 import (
+	"context"
+	"os"
 	"testing"
+	"time"
 
+	"github.com/brianvoe/gofakeit/v6"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func getPsqlApi(t *testing.T) (*PsqlApi, error) {
+	t.Helper()
+
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	host := os.Getenv("POSTGRES_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	t.Logf("using postres host: %s", host)
+
+	return NewNetlogPsqlApi(timeoutCtx, host, "5432", "serj_blogs")
+}
 
 func TestUtil_getQueryLikeCondition(t *testing.T) {
 	// no keywords
@@ -30,8 +52,64 @@ func TestUtil_getQueryLikeCondition(t *testing.T) {
 	assert.Equal(t, "WHERE title LIKE '%word1%' AND title LIKE '%word2%' AND title LIKE '%word3%' AND source = 'pc'", queryLike)
 }
 
+func TestNewNetlogPsqlApi(t *testing.T) {
+	psqlApi, err := getPsqlApi(t)
+	require.NoError(t, err)
+	require.NotNil(t, psqlApi)
+	assert.NotNil(t, psqlApi.db)
+}
+
 func TestPsqlApi_AddVisit(t *testing.T) {
-	// TODO: maybe add some tests against the real DB
-	// PostgreSQL containers on GitHub Actions:
-	//	https://docs.github.com/en/actions/guides/creating-postgresql-service-containers
+	ctx := context.Background()
+	psqlApi, err := getPsqlApi(t)
+	require.NoError(t, err)
+
+	count, err := psqlApi.CountAll(ctx)
+	require.NoError(t, err)
+
+	err = psqlApi.AddVisit(ctx, &Visit{})
+	assert.Equal(t, "visit url or timestamp empty", err.Error())
+	err = psqlApi.AddVisit(ctx, &Visit{
+		Title:     "test",
+		Source:    "pc",
+		URL:       "",
+		Timestamp: time.Now(),
+	})
+	assert.Equal(t, "visit url or timestamp empty", err.Error())
+
+	now := time.Now()
+	v1 := &Visit{
+		Title:     gofakeit.Name(),
+		Source:    "pc",
+		URL:       gofakeit.URL(),
+		Timestamp: now,
+	}
+	v2 := &Visit{
+		Title:     gofakeit.Name(),
+		Source:    "safari",
+		URL:       gofakeit.URL(),
+		Timestamp: now,
+	}
+	v3 := &Visit{
+		Title:     gofakeit.Name(),
+		Source:    "chrome",
+		URL:       gofakeit.URL(),
+		Timestamp: now,
+	}
+
+	require.NoError(t, psqlApi.AddVisit(ctx, v1))
+	require.NoError(t, psqlApi.AddVisit(ctx, v2))
+	require.NoError(t, psqlApi.AddVisit(ctx, v3))
+
+	assert.NotEqual(t, v1.Id, v2.Id)
+	assert.NotEqual(t, v1.Id, v3.Id)
+	assert.NotEqual(t, v2.Id, v3.Id)
+	assert.True(t, now.Equal(v1.Timestamp), "%v should be before %v", now, v1.Timestamp)
+	assert.True(t, now.Equal(v2.Timestamp), "%v should be before %v", now, v2.Timestamp)
+	assert.True(t, now.Equal(v2.Timestamp), "%v should be before %v", now, v3.Timestamp)
+
+	countAfter, err := psqlApi.CountAll(ctx)
+	require.NoError(t, err)
+
+	assert.Equal(t, 3, countAfter-count)
 }
