93782542472bfb5e1d5b97519aeb3672bd7b8bb3
Serj Tubin <stubin87@gmail.com>
2022-11-26 10:38:02 +0100

    creating otel tracer added to telemetry/tracing/tracing.go; not used, as it does not work

--- internal/telemetry/tracing/tracing.go
+++ internal/telemetry/tracing/tracing.go
@@ -1,74 +1,184 @@
 package tracing
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+
+	"go.opentelemetry.io/otel/attribute"
+	semconv "go.opentelemetry.io/otel/semconv/v1.10.0"
+
+	"go.opentelemetry.io/contrib/propagators/b3"
+	"go.opentelemetry.io/contrib/propagators/ot"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/encoding/gzip"
 )
 
 var GlobalTracer = otel.Tracer("main-backend")
 var GlobalNetlogBackupTracer = otel.Tracer("gdrive-netlog-backup")
 
-// replacing it for: https://github.com/exaring/otelpgx
+func GetDefaultTraceResource(
+	ctx context.Context,
+	serviceName, serviceVersion string,
+	resourceAttributes map[string]string,
+) (*resource.Resource, error) {
+	r := resource.Environment()
 
-//type PgxOtelTracer struct {
-//	tracer         trace.Tracer
-//	tracingEnabled bool
-//}
+	hostnameSet := false
+	for iter := r.Iter(); iter.Next(); {
+		if iter.Attribute().Key == semconv.HostNameKey && len(iter.Attribute().Value.Emit()) > 0 {
+			hostnameSet = true
+		}
+	}
+
+	attributes := []attribute.KeyValue{
+		semconv.TelemetrySDKNameKey.String("launcher"),
+		semconv.TelemetrySDKLanguageGo,
+	}
+
+	if serviceName != "" {
+		attributes = append(attributes, semconv.ServiceNameKey.String(serviceName))
+	}
+
+	if serviceVersion != "" {
+		attributes = append(attributes, semconv.ServiceVersionKey.String(serviceVersion))
+	}
+
+	for key, value := range resourceAttributes {
+		if len(value) > 0 {
+			if key == string(semconv.HostNameKey) {
+				hostnameSet = true
+			}
+			attributes = append(attributes, attribute.String(key, value))
+		}
+	}
+
+	if !hostnameSet {
+		hostname, err := os.Hostname()
+		if err != nil {
+			return nil, fmt.Errorf("unable to set host.name. Set OTEL_RESOURCE_ATTRIBUTES=\"host.name=<your_host_name>\" env var or configure WithResourceAttributes in code: %w", err)
+		}
+		attributes = append(attributes, semconv.HostNameKey.String(hostname))
+	}
+
+	attributes = append(r.Attributes(), attributes...)
+
+	var err error
+	r, err = resource.New(
+		ctx,
+		resource.WithSchemaURL(semconv.SchemaURL),
+		resource.WithAttributes(attributes...),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// Note: There are new detectors we may wish to take advantage
+	// of, now available in the default SDK (e.g., WithProcess(),
+	// WithOSType(), ...).
+	return r, nil
+}
+
+// SetupTracing can be used as:
+// defResource, err := tracing.GetDefaultTraceResource(
 //
-//func NewPgxOtelTracer(tracingEnabled bool, tracer trace.Tracer) *PgxOtelTracer {
-//	return &PgxOtelTracer{
-//		tracingEnabled: tracingEnabled,
-//		tracer:         tracer,
-//	}
-//}
-//
-//func (t *PgxOtelTracer) TraceConnectStart(ctx context.Context, data pgx.TraceConnectStartData) context.Context {
-//	if !t.tracingEnabled {
-//		return ctx
-//	}
-//	ctx, span := t.tracer.Start(ctx, "db.connectStart")
-//	defer span.End()
-//	return ctx
-//}
-//
-//func (t *PgxOtelTracer) TraceConnectEnd(ctx context.Context, data pgx.TraceConnectEndData) {
-//	if !t.tracingEnabled {
-//		return
-//	}
-//
-//	ctx, span := t.tracer.Start(ctx, "db.connectEnd")
-//	defer span.End()
-//
-//	if data.Err != nil {
-//		span.SetStatus(codes.Error, data.Err.Error())
-//		span.RecordError(data.Err)
-//	}
-//}
-//
-//func (t *PgxOtelTracer) TraceQueryStart(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryStartData) context.Context {
-//	if !t.tracingEnabled {
-//		return ctx
-//	}
-//
-//	ctx, span := t.tracer.Start(ctx, "db.queryStart")
-//	defer span.End()
-//
-//	span.SetAttributes(attribute.String("sql", data.SQL))
-//	//span.SetAttributes(attribute.String("sql_args", data.Args))
-//
-//	return ctx
-//}
-//
-//func (t *PgxOtelTracer) TraceQueryEnd(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryEndData) {
-//	if !t.tracingEnabled {
-//		return
-//	}
-//
-//	ctx, span := t.tracer.Start(ctx, "db.queryEnd")
-//	defer span.End()
-//
-//	span.SetAttributes(attribute.String("commandTag", data.CommandTag.String()))
-//	if data.Err != nil {
-//		span.SetStatus(codes.Error, data.Err.Error())
-//		span.RecordError(data.Err)
-//	}
-//}
+//		ctx, "serj-tubin-com", "v1", map[string]string{},
+//	)
+//	otelShutdown, err := tracing.SetupTracing(ctx, defResource)
+func SetupTracing(
+	ctx context.Context,
+	resources ...*resource.Resource,
+) (func(timeoutCtx context.Context) error, error) {
+	// FIXME: just does not work... does not send traces, not sure why
+
+	honeycombApiKey := os.Getenv("HONEYCOMB_API_KEY")
+	if honeycombApiKey == "" {
+		return nil, errors.New("honeycomb api key not set")
+	}
+
+	opts := []trace.TracerProviderOption{
+		//trace.WithSampler(c.Sampler),
+		trace.WithSampler(trace.AlwaysSample()),
+	}
+
+	for _, r := range resources {
+		opts = append(opts, trace.WithResource(r))
+	}
+
+	spanProcessors := []trace.SpanProcessor{}
+	for _, sp := range spanProcessors {
+		opts = append(opts, trace.WithSpanProcessor(sp))
+	}
+
+	// make sure the exporter is added last
+	endpoint, insecure := "api.honeycomb.io:443", false
+	spanExporter, err := newGRPCTraceExporter(ctx, endpoint, insecure, map[string]string{
+		"x-honeycomb-team": honeycombApiKey,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create span exporter: %v", err)
+	}
+
+	bsp := trace.NewBatchSpanProcessor(spanExporter)
+	opts = append(opts, trace.WithSpanProcessor(bsp))
+
+	tp := trace.NewTracerProvider(opts...)
+	if err = configurePropagators([]string{
+		"tracecontext", "baggage",
+	}); err != nil {
+		return nil, err
+	}
+
+	otel.SetTracerProvider(tp)
+
+	return func(timeoutCtx context.Context) error {
+		_ = bsp.Shutdown(context.Background())
+		return spanExporter.Shutdown(context.Background())
+	}, nil
+}
+
+func newGRPCTraceExporter(ctx context.Context, endpoint string, insecure bool, headers map[string]string) (*otlptrace.Exporter, error) {
+	secureOption := otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
+	if insecure {
+		secureOption = otlptracegrpc.WithInsecure()
+	}
+	return otlptrace.New(
+		ctx,
+		otlptracegrpc.NewClient(
+			secureOption,
+			otlptracegrpc.WithEndpoint(endpoint),
+			otlptracegrpc.WithHeaders(headers),
+			otlptracegrpc.WithCompressor(gzip.Name),
+		),
+	)
+}
+
+func configurePropagators(propagators []string) error {
+	propagatorsMap := map[string]propagation.TextMapPropagator{
+		"b3":           b3.New(b3.WithInjectEncoding(b3.B3MultipleHeader)),
+		"baggage":      propagation.Baggage{},
+		"tracecontext": propagation.TraceContext{},
+		"ottrace":      ot.OT{},
+	}
+	var props []propagation.TextMapPropagator
+	for _, key := range propagators {
+		prop := propagatorsMap[key]
+		if prop != nil {
+			props = append(props, prop)
+		}
+	}
+	if len(props) == 0 {
+		return fmt.Errorf("invalid configuration: unsupported propagators. Supported options: b3,baggage,tracecontext,ottrace")
+	}
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(
+		props...,
+	))
+	return nil
+}
