2a130a5f1b5658de21197856f66c6249d4b2c1c2
Serj Tubin <serj.tubin@sumup.com>
2023-04-15 11:27:48 +0200

    blog handler json req

--- internal/blog/handler.go
+++ internal/blog/handler.go
@@ -14,6 +14,21 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+type clapBlogRequest struct {
+	ID int `json:"id"`
+}
+
+type newBlogRequest struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+type updateBlogRequest struct {
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
 type Handler struct {
 	blogApi      Api
 	loginChecker auth.Checker
@@ -41,29 +56,38 @@ func (handler *Handler) SetupRoutes(blogRouter *mux.Router) {
 }
 
 func (handler *Handler) handleNewBlog(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		log.Errorf("add new blog failed, parse form error: %s", err)
-		http.Error(w, "parse form error", http.StatusInternalServerError)
-		return
+	var newBlogReq newBlogRequest
+	if r.Header.Get("Content-Type") == "application/json" {
+		if err := json.NewDecoder(r.Body).Decode(&newBlogReq); err != nil {
+			log.Errorf("new blog, unmarshal json params: %s", err)
+			http.Error(w, "add blog failed", http.StatusBadRequest)
+			return
+		}
+	} else {
+		if err := r.ParseForm(); err != nil {
+			log.Errorf("add new blog failed, parse form error: %s", err)
+			http.Error(w, "parse form error", http.StatusInternalServerError)
+			return
+		}
+		newBlogReq = newBlogRequest{
+			Title:   r.Form.Get("title"),
+			Content: r.Form.Get("content"),
+		}
 	}
 
-	title := r.Form.Get("title")
-	if title == "" {
+	if newBlogReq.Title == "" {
 		http.Error(w, "error, title empty", http.StatusBadRequest)
 		return
 	}
-
-	content := r.Form.Get("content")
-	if content == "" {
+	if newBlogReq.Content == "" {
 		http.Error(w, "error, content empty", http.StatusBadRequest)
 		return
 	}
 
 	newBlog := &Blog{
-		Title:     title,
+		Title:     newBlogReq.Title,
+		Content:   newBlogReq.Content,
 		CreatedAt: time.Now(),
-		Content:   content,
 	}
 
 	if err := handler.blogApi.AddBlog(r.Context(), newBlog); err != nil {
@@ -79,42 +103,52 @@ func (handler *Handler) handleNewBlog(w http.ResponseWriter, r *http.Request) {
 }
 
 func (handler *Handler) handleUpdateBlog(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		log.Errorf("update blog failed, parse form error: %s", err)
-		http.Error(w, "parse form error", http.StatusInternalServerError)
-		return
+	var updateBlogReq updateBlogRequest
+	if r.Header.Get("Content-Type") == "application/json" {
+		if err := json.NewDecoder(r.Body).Decode(&updateBlogReq); err != nil {
+			log.Errorf("update blog, unmarshal json params: %s", err)
+			http.Error(w, "update blog failed", http.StatusBadRequest)
+			return
+		}
+	} else {
+		if err := r.ParseForm(); err != nil {
+			log.Errorf("update blog failed, parse form error: %s", err)
+			http.Error(w, "parse form error", http.StatusInternalServerError)
+			return
+		}
+		idStr := r.Form.Get("id")
+		if idStr == "" {
+			http.Error(w, "error, id empty", http.StatusBadRequest)
+			return
+		}
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, "error, id NaN", http.StatusBadRequest)
+			return
+		}
+		updateBlogReq = updateBlogRequest{
+			ID:      id,
+			Title:   r.Form.Get("title"),
+			Content: r.Form.Get("content"),
+		}
 	}
 
-	idStr := r.Form.Get("id")
-	if idStr == "" {
-		http.Error(w, "error, id empty", http.StatusBadRequest)
-		return
-	}
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, "error, id NaN", http.StatusBadRequest)
-		return
-	}
-
-	title := r.Form.Get("title")
-	if title == "" {
+	if updateBlogReq.Title == "" {
 		http.Error(w, "error, title empty", http.StatusBadRequest)
 		return
 	}
-
-	content := r.Form.Get("content")
-	if content == "" {
+	if updateBlogReq.Content == "" {
 		http.Error(w, "error, content empty", http.StatusBadRequest)
 		return
 	}
 
-	if err := handler.blogApi.UpdateBlog(r.Context(), id, title, content); err != nil {
+	if err := handler.blogApi.UpdateBlog(r.Context(), updateBlogReq.ID, updateBlogReq.Title, updateBlogReq.Content); err != nil {
 		log.Errorf("update blog failed: %s", err)
 		http.Error(w, "update blog failed", http.StatusInternalServerError)
 		return
 	}
 
-	pkg.WriteResponse(w, "", fmt.Sprintf("updated:%d", id))
+	pkg.WriteResponse(w, "", fmt.Sprintf("updated:%d", updateBlogReq.ID))
 }
 
 func (handler *Handler) handleBlogClapped(w http.ResponseWriter, r *http.Request) {
@@ -124,24 +158,36 @@ func (handler *Handler) handleBlogClapped(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	idStr := r.Form.Get("id")
-	if idStr == "" {
-		http.Error(w, "error, id empty", http.StatusBadRequest)
-		return
-	}
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, "error, id NaN", http.StatusBadRequest)
-		return
+	var clapBlogReq clapBlogRequest
+	if r.Header.Get("Content-Type") == "application/json" {
+		if err := json.NewDecoder(r.Body).Decode(&clapBlogReq); err != nil {
+			log.Errorf("blog clap, unmarshal json params: %s", err)
+			http.Error(w, "update blog failed", http.StatusBadRequest)
+			return
+		}
+	} else {
+		idStr := r.Form.Get("id")
+		if idStr == "" {
+			http.Error(w, "error, id empty", http.StatusBadRequest)
+			return
+		}
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, "error, id NaN", http.StatusBadRequest)
+			return
+		}
+		clapBlogReq = clapBlogRequest{
+			ID: id,
+		}
 	}
 
-	if err := handler.blogApi.BlogClapped(r.Context(), id); err != nil {
+	if err := handler.blogApi.BlogClapped(r.Context(), clapBlogReq.ID); err != nil {
 		log.Errorf("update blog failed: %s", err)
 		http.Error(w, "update blog failed", http.StatusInternalServerError)
 		return
 	}
 
-	pkg.WriteResponse(w, "", fmt.Sprintf("updated:%d", id))
+	pkg.WriteResponse(w, "", fmt.Sprintf("updated:%d", clapBlogReq.ID))
 }
 
 func (handler *Handler) handleDeleteBlog(w http.ResponseWriter, r *http.Request) {
