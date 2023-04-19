edd6885df2165e5ffdd935894a285ca7fd365335
Serj Tubin <stubin87@gmail.com>
2023-04-19 14:35:35 +0200

    docker: milion scripts - 1 makefile

--- docker/rebuild-file-service.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker-compose stop -t 1 fileservice
-docker-compose build fileservice
-docker-compose up --no-start fileservice
-docker-compose start fileservice
--- docker/up-prometheus-misc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker-compose -p serj-tubin-com-prometheus -f docker-compose-prometheus.yml up -d
