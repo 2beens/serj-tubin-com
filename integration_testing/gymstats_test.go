16aecae5e214053f2b9a66fbe8b9c912ea64d0d1
serj.tubin <stubin87@gmail.com>
2023-05-13 21:51:11 +0200

    int tests suite

---
+++ integration_testing/gymstats_test.go
@@ -0,0 +1,31 @@
+package integration_testing
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_GymStats_HappyPaths(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	suite := newSuite(ctx)
+	defer suite.cleanup()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("%s/gymstats/list", serverEndpoint), nil)
+	require.NoError(t, err)
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+	defer resp.Body.Close()
+
+	respBytes, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	assert.Equal(t, "test-version-info", string(respBytes))
+}
