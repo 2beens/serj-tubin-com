a51c84e49a5224a8e014342290b4680f4f2bae45
serj.tubin <stubin87@gmail.com>
2023-05-14 12:24:11 +0200

    Test_Login updated

--- test/auth_test.go
+++ test/auth_test.go
@@ -7,12 +7,18 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/2beens/serjtubincom/internal/misc"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+type loginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
 func Test_Login(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -21,30 +27,74 @@ func Test_Login(t *testing.T) {
 	suite := newSuite(ctx)
 	defer suite.cleanup()
 
-	loginRequest := struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
+	cases := map[string]struct {
+		loginReq           loginRequest
+		expectedStatusCode int
+		assertFunc         func(resp *http.Response)
 	}{
-		Username: testUsername,
-		Password: testPassword,
+		"good creds": {
+			loginReq: loginRequest{
+				Username: testUsername,
+				Password: testPassword,
+			},
+			expectedStatusCode: http.StatusOK,
+			assertFunc: func(resp *http.Response) {
+				respBytes, err := io.ReadAll(resp.Body)
+				require.NoError(t, err)
+
+				var loginResp misc.LoginResponse
+				require.NoError(t, json.Unmarshal(respBytes, &loginResp))
+				assert.NotEmpty(t, loginResp.Token)
+			},
+		},
+		"bad password": {
+			loginReq: loginRequest{
+				Username: testUsername,
+				Password: "bad-password",
+			},
+			expectedStatusCode: http.StatusBadRequest,
+			assertFunc: func(resp *http.Response) {
+				respBytes, err := io.ReadAll(resp.Body)
+				require.NoError(t, err)
+				respString := strings.TrimSpace(string(respBytes))
+				assert.Equal(t, "login failed", respString)
+			},
+		},
+		"bad username": {
+			loginReq: loginRequest{
+				Username: "bad-username",
+				Password: testPassword,
+			},
+			expectedStatusCode: http.StatusBadRequest,
+			assertFunc: func(resp *http.Response) {
+				respBytes, err := io.ReadAll(resp.Body)
+				require.NoError(t, err)
+				respString := strings.TrimSpace(string(respBytes))
+				assert.Equal(t, "login failed", respString)
+			},
+		},
 	}
-	loginReqJson, err := json.Marshal(loginRequest)
-	require.NoError(t, err)
 
-	req, err := http.NewRequestWithContext(ctx, "POST", fmt.Sprintf("%s/a/login", serverEndpoint), bytes.NewBuffer(loginReqJson))
-	require.NoError(t, err)
-	req.Header.Set("User-Agent", "test-agent")
-	req.Header.Set("Content-Type", "application/json")
+	for tn, tc := range cases {
+		t.Run(tn, func(t *testing.T) {
+			loginRequest := loginRequest{
+				Username: tc.loginReq.Username,
+				Password: tc.loginReq.Password,
+			}
+			loginReqJson, err := json.Marshal(loginRequest)
+			require.NoError(t, err)
 
-	resp, err := http.DefaultClient.Do(req)
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, resp.StatusCode)
-	defer resp.Body.Close()
+			req, err := http.NewRequestWithContext(ctx, "POST", fmt.Sprintf("%s/a/login", serverEndpoint), bytes.NewBuffer(loginReqJson))
+			require.NoError(t, err)
+			req.Header.Set("User-Agent", "test-agent")
+			req.Header.Set("Content-Type", "application/json")
 
-	respBytes, err := io.ReadAll(resp.Body)
-	require.NoError(t, err)
+			resp, err := http.DefaultClient.Do(req)
+			require.NoError(t, err)
+			require.Equal(t, tc.expectedStatusCode, resp.StatusCode)
+			defer resp.Body.Close()
 
-	var loginResp misc.LoginResponse
-	require.NoError(t, json.Unmarshal(respBytes, &loginResp))
-	assert.NotEmpty(t, loginResp.Token)
+			tc.assertFunc(resp)
+		})
+	}
 }
