372586f93a59e1720308447fdedbd65cb61907b2
serj.tubin <stubin87@gmail.com>
2024-03-17 20:47:20 +0100

    sql: add basic exercises

--- sql/db_schema.sql
+++ sql/db_schema.sql
@@ -104,3 +104,101 @@ CREATE TABLE public.visitor_board_message
 
 ALTER TABLE public.visitor_board_message OWNER TO postgres;
 CREATE INDEX ix_visitor_board_message_created_at ON public.visitor_board_message (created_at);
+
+-- add basic exercise types from the javascript code from the web client:
+---------------------------------------------------------------------------
+-- biceps exercises
+INSERT INTO public.exercise_type (id, muscle_group, name, created_at)
+VALUES
+    ('preacher_curl', 'biceps', 'Preacher Curl', NOW()),
+    ('barbell_curl', 'biceps', 'Barbell Curl', NOW()),
+    ('barbell_vertical_curl', 'biceps', 'Barbell Curl [vertical grip]', NOW()),
+    ('ez_bar_curl', 'biceps', 'EZ Bar Curl', NOW()),
+    ('ez_bar_curl_declined', 'biceps', 'EZ Bar Curl [declined]', NOW()),
+    ('dumbells', 'biceps', 'Dumbells', NOW()),
+    ('dumbells_inclined', 'biceps', 'Dumbells [inclined]', NOW()),
+    ('dumbells_declined', 'biceps', 'Dumbells [declined]', NOW());
+
+-- triceps exercises
+INSERT INTO public.exercise_type (id, muscle_group, name, created_at)
+VALUES
+    ('skullcrusher_with_ez_bar', 'triceps', 'Skullcrusher w EZ 💀', NOW()),
+    ('triceps_pushdown', 'triceps', 'Tricep Pushdown', NOW()),
+    ('skull_crushers', 'triceps', 'Skull Crushers', NOW()),
+    ('close_grip_bench_press', 'triceps', 'Close-Grip Bench Press', NOW()),
+    ('bench_dip', 'triceps', 'Bench Dip', NOW()),
+    ('dumbbell_overhead_triceps_extension', 'triceps', 'Dumbbell Overhead Triceps Extension', NOW()),
+    ('cable_overhead_extension_with_rope', 'triceps', 'Cable Overhead Extension With Rope', NOW()),
+    ('cable_push_down', 'triceps', 'Cable Push-Down', NOW()),
+    ('barbell_push_down', 'triceps', 'Barbell Push-Down', NOW()),
+    ('barbell_push_down_ez', 'triceps', 'Barbell EZ Push-Down', NOW());
+
+-- chest exercises
+INSERT INTO public.exercise_type (id, muscle_group, name, created_at)
+VALUES
+    ('bench_press', 'chest', 'Bench Press', NOW()),
+    ('bench_press_inclined', 'chest', 'Bench Press [inclined]', NOW()),
+    ('bench_press_declined', 'chest', 'Bench Press [declined]', NOW()),
+    ('dips', 'chest', 'Dips', NOW()),
+    ('chest_fly_machine', 'chest', 'Chest Fly [machine]', NOW()),
+    ('chest_fly_machine_inclined', 'chest', 'Chest Fly [machine][inclined]', NOW()),
+    ('chest_press_machine', 'chest', 'Chest Press [machine]', NOW()),
+    ('pushups', 'chest', 'Push-Ups', NOW()),
+    ('dumbbell_pull_over', 'chest', 'Dumbbell Pull-Over', NOW());
+
+-- legs exercises
+INSERT INTO public.exercise_type (id, muscle_group, name, created_at)
+VALUES
+    ('leg_press', 'legs', 'Leg Press', NOW()),
+    ('calf_raise_seated', 'legs', 'Calf Raise [seated]', NOW()),
+    ('calf_raise_standing', 'legs', 'Calf Raise [standing]', NOW()),
+    ('walking_dumbbell_lunges', 'legs', 'Walking Dumbbell Lunges', NOW()),
+    ('leg_extension_machine', 'legs', 'Leg Extension Machine', NOW()),
+    ('leg_curl_machine', 'legs', 'Leg Curl Machine', NOW());
+
+-- shoulders exercises
+INSERT INTO public.exercise_type (id, muscle_group, name, created_at)
+VALUES
+    ('lateral_raise', 'shoulders', 'Lateral Raise 👐', NOW()),
+    ('side_lateral_raise', 'shoulders', 'Side Lateral Raise 👐', NOW()),
+    ('lateral_raise_single_arm_cable', 'shoulders', 'Lateral Raise 👐 [single] [cable]', NOW()),
+    ('front_raise', 'shoulders', 'Front Raise ➬', NOW()),
+    ('front_raise_single_arm_cable', 'shoulders', 'Front Raise 👐 [single] [cable]', NOW()),
+    ('back_raise', 'shoulders', 'Back Raise 🔙', NOW()),
+    ('back_push_machine', 'shoulders', 'Back Push 🔙 [machine]', NOW()),
+    ('press_barbell_standing', 'shoulders', 'Press [barbell] [standing]', NOW()),
+    ('press_dumbell_standing', 'shoulders', 'Press [dumbell] [standing]', NOW()),
+    ('press_barbell_seated', 'shoulders', 'Press [barbell] [seated]', NOW()),
+    ('press_dumbell_seated', 'shoulders', 'Press [dumbell] [seated]', NOW()),
+    ('arnold_press', 'shoulders', 'Arnold Press', NOW()),
+    ('rear_delt_fly', 'shoulders', 'Rear Delt Fly', NOW());
+
+-- back exercises
+INSERT INTO public.exercise_type (id, muscle_group, name, created_at)
+VALUES
+    ('dumbbell_row_inclined', 'back', 'Inclined Dumbbell Row', NOW()),
+    ('barbell_row_inclined', 'back', 'Inclined Barbell Row', NOW()),
+    ('single_arm_dumbell_row', 'back', 'Single-Arm Dumbell Row', NOW()),
+    ('bent_over_row', 'back', 'Bent Over Row', NOW()),
+    ('t_bar_row', 'back', 'T-Bar Row', NOW()),
+    ('pull_up', 'back', 'Pull Up', NOW()),
+    ('seated_row_barbell', 'back', 'Seated Row [barbell]', NOW()),
+    ('seated_row_v_handle', 'back', 'Seated Row [V handle]', NOW()),
+    ('hyperextensions', 'back', 'Hyperextensions', NOW()),
+    ('lat_pull_down_barbell', 'back', 'Lat Pull-Down [barbell]', NOW()),
+    ('lat_pull_down_v_handle', 'back', 'Lat Pull-Down [V handle]', NOW());
+
+-- other exercises
+INSERT INTO public.exercise_type (id, muscle_group, name, created_at)
+VALUES
+    ('oblique_crunch_hyperext_bench', 'other', 'Oblique Crunch [Hyperextension Bench]', NOW()),
+    ('crunch', 'other', 'Crunch', NOW()),
+    ('tuck_crunch', 'other', 'Tuck Crunch', NOW()),
+    ('leg_raise_core', 'other', 'Leg Raise [core]', NOW()),
+    ('squat', 'other', 'Squat', NOW()),
+    ('squat_jump', 'other', 'Squat Jump', NOW()),
+    ('burpee', 'other', 'Burpee', NOW()),
+    ('plank', 'other', 'Plank', NOW()),
+    ('hanging_knee_raise', 'other', 'Hanging Knee Raise', NOW()),
+    ('v_sit', 'other', 'V-sit', NOW()),
+    ('test', 'other', '🛠️Test🛠️[dummy]🛠️', NOW());
