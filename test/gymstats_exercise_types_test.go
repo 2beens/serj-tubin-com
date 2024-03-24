37d92168dcee1e7dfdd5dba6cbd2feefb4d5e7cd
Serj Tubin <stubin87@gmail.com>
2024-03-24 13:05:53 +0100

    add ex type int. tests

--- test/gymstats_exercise_types_test.go
+++ test/gymstats_exercise_types_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -13,6 +14,45 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func (s *IntegrationTestSuite) addExerciseTypeRequest(
+	ctx context.Context,
+	authToken string,
+	exType exercises.ExerciseType,
+	expectedStatusCode int,
+) {
+	exTypeBytes, err := json.Marshal(exType)
+	require.NoError(s.T(), err)
+
+	req, err := http.NewRequestWithContext(
+		ctx,
+		"POST",
+		fmt.Sprintf("%s/gymstats/types", serverEndpoint),
+		bytes.NewReader(exTypeBytes),
+	)
+	require.NoError(s.T(), err)
+	req.Header.Set("User-Agent", "test-agent")
+	req.Header.Set("X-SERJ-TOKEN", authToken)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := s.httpClient.Do(req)
+	require.NoError(s.T(), err)
+	if resp.StatusCode != expectedStatusCode {
+		respBytes, err := io.ReadAll(resp.Body)
+		require.NoError(s.T(), err)
+		fmt.Printf(
+			"unexpected status code: %d, response body: %s\n",
+			resp.StatusCode, string(respBytes),
+		)
+		require.Equal(s.T(), expectedStatusCode, resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	respBytes, err := io.ReadAll(resp.Body)
+	require.NoError(s.T(), err)
+
+	fmt.Printf("add ex. type, response body: %s\n", string(respBytes))
+}
 
 func (s *IntegrationTestSuite) getAllExerciseTypesRequest(
 	ctx context.Context,
@@ -96,7 +136,55 @@ func (s *IntegrationTestSuite) TestGymStats_ExerciseTypes() {
 	})
 
 	s.T().Run("add exercise type", func(t *testing.T) {
-		// TODO:
-		t.Skip("not implemented")
+		newExType := exercises.ExerciseType{
+			ID:          "some_id",
+			MuscleGroup: "legs",
+			Name:        "Some Ex1",
+			Description: "Some Ex1 description",
+		}
+
+		s.addExerciseTypeRequest(ctx, authToken, newExType, http.StatusCreated)
+
+		// try to get it now
+		addedExType := s.getAllExerciseTypesRequest(ctx, authToken,
+			exercises.GetExerciseTypesParams{
+				MuscleGroup: newExType.MuscleGroup,
+				ExerciseId:  newExType.ID,
+			},
+			http.StatusOK,
+		)
+		require.Len(t, addedExType, 1)
+		assert.Equal(t, newExType.ID, addedExType[0].ID)
+		assert.Equal(t, newExType.MuscleGroup, addedExType[0].MuscleGroup)
+		assert.Equal(t, newExType.Name, addedExType[0].Name)
+		assert.Equal(t, newExType.Description, addedExType[0].Description)
+		assert.False(t, addedExType[0].CreatedAt.IsZero())
+
+		// try to add the same exercise type again
+		s.addExerciseTypeRequest(ctx, authToken, newExType, http.StatusConflict)
+
+		// same id but different group
+		newExType2 := exercises.ExerciseType{
+			ID:          "some_id",
+			MuscleGroup: "shoulders",
+			Name:        "Some Ex2",
+			Description: "Some Ex2 description",
+		}
+		s.addExerciseTypeRequest(ctx, authToken, newExType2, http.StatusCreated)
+
+		// try to get it now
+		addedExType2 := s.getAllExerciseTypesRequest(ctx, authToken,
+			exercises.GetExerciseTypesParams{
+				MuscleGroup: newExType2.MuscleGroup,
+				ExerciseId:  newExType2.ID,
+			},
+			http.StatusOK,
+		)
+		require.Len(t, addedExType2, 1)
+		assert.Equal(t, newExType2.ID, addedExType2[0].ID)
+		assert.Equal(t, newExType2.MuscleGroup, addedExType2[0].MuscleGroup)
+		assert.Equal(t, newExType2.Name, addedExType2[0].Name)
+		assert.Equal(t, newExType2.Description, addedExType2[0].Description)
+		assert.False(t, addedExType2[0].CreatedAt.IsZero())
 	})
 }
