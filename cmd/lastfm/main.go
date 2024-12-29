957ca507f62b25a13f2d83198703ff7cb906095b
Serj Tubin <stubin87@gmail.com>
2024-12-29 22:49:55 +0100

    verbose log

--- cmd/lastfm/main.go
+++ cmd/lastfm/main.go
@@ -23,7 +23,7 @@ func main() {
 	defer cancel()
 
 	// Parse and validate the input
-	host, port, dbName, jsonPath, err := parseAndValidateInput()
+	host, port, dbName, jsonPath, verbose, err := parseAndValidateInput()
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
@@ -53,6 +53,7 @@ func main() {
 	}
 
 	// Iterate over tracks, map them to Spotify tracks, and insert them into the database
+	var failedInserts []track
 	for _, wrapper := range lfmRoot {
 		for _, lfmTrack := range wrapper.Track {
 			spotifyTrack, err := mapLastFMTrackToSpotifyTrack(lfmTrack)
@@ -63,11 +64,28 @@ func main() {
 
 			// Insert track into DB
 			if err = repo.Add(ctx, spotifyTrack); err != nil {
-				log.Printf("--- Failed to insert track %+v: %v", spotifyTrack, err)
-			} else {
+				log.Printf(
+					"--- Failed to insert track [%s - %s] [played at: %s]: %v\n",
+					spotifyTrack.Artists, spotifyTrack.Name, spotifyTrack.PlayedAt, err,
+				)
+				failedInserts = append(failedInserts, lfmTrack)
+			} else if verbose {
 				log.Printf("+++ Inserted track: %+v", spotifyTrack)
 			}
 		}
+	}
+
+	// finally, print the failed inserts as json so we can investigate them separately and fix them
+	if len(failedInserts) > 0 {
+		failedInsertsJSON, err := json.MarshalIndent(failedInserts, "", "  ")
+		if err != nil {
+			log.Fatalf("Failed to marshal failed inserts: %v\n", err)
+		}
+		log.Println("----------------------------------------------------")
+		log.Println("----------------------------------------------------")
+		log.Println("")
+		log.Printf("Failed inserts below: \n")
+		log.Println(string(failedInsertsJSON))
 	}
 }
 
@@ -89,35 +107,36 @@ func getRepo(ctx context.Context, port string, host string, dbName string) (*spo
 	return spotify.NewRepo(dbPool), nil
 }
 
-func parseAndValidateInput() (string, string, string, string, error) {
+func parseAndValidateInput() (string, string, string, string, bool, error) {
 	// Define flags for PostgreSQL host, port, database name, and JSON file path
 	host := flag.String("host", "", "PostgreSQL host (e.g., localhost or IP address)")
 	port := flag.String("port", "", "PostgreSQL port (e.g., 5432)")
 	dbName := flag.String("dbname", "", "PostgreSQL database name")
 	jsonPath := flag.String("json", "", "Path to the JSON file with LastFM data")
+	verbose := flag.Bool("verbose", false, "Verbose output")
 
 	// Parse the flags
 	flag.Parse()
 
 	// Validate required inputs
 	if *host == "" {
-		return "", "", "", "", fmt.Errorf("PostgreSQL host is required (use -host)")
+		return "", "", "", "", false, fmt.Errorf("PostgreSQL host is required (use -host)")
 	}
 	if *port == "" {
-		return "", "", "", "", fmt.Errorf("PostgreSQL port is required (use -port)")
+		return "", "", "", "", false, fmt.Errorf("PostgreSQL port is required (use -port)")
 	}
 	if *dbName == "" {
-		return "", "", "", "", fmt.Errorf("PostgreSQL database name is required (use -dbname)")
+		return "", "", "", "", false, fmt.Errorf("PostgreSQL database name is required (use -dbname)")
 	}
 	if *jsonPath == "" {
-		return "", "", "", "", fmt.Errorf("Path to JSON file is required (use -json)")
+		return "", "", "", "", false, fmt.Errorf("Path to JSON file is required (use -json)")
 	}
 
 	// Check if the JSON file exists
 	if _, err := os.Stat(*jsonPath); os.IsNotExist(err) {
-		return "", "", "", "", fmt.Errorf("JSON file does not exist at path: %s", *jsonPath)
+		return "", "", "", "", false, fmt.Errorf("JSON file does not exist at path: %s", *jsonPath)
 	}
 
 	// Return the validated inputs
-	return *host, *port, *dbName, *jsonPath, nil
+	return *host, *port, *dbName, *jsonPath, *verbose, nil
 }
